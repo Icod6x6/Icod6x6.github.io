7741389fe1827c22b4af944e067581d52064570d
Icod6x6 <59237336+Icod6x6@users.noreply.github.com>
2019-12-29 21:40:32 +1200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,13 +1,24 @@
 <?php
-
-//Taking all values
+ 
 $fname 		= $_POST['fname'];
 $email 		= $_POST['email'];
 $subject 	= $_POST['subject'];
 $msg 		= $_POST['msg'];
-$output 	= "Name: ".$fname."\n\nSubject: ".$subject."\n\nMessage: ".$msg;
 
-$to 		= 'satimudesigns@gmail.com';
-$headers	= 'FROM: "'.$email.'"';
+$email_from = 'satimudesigns';
 
-$send		= mail($to, $fname, $output."\n\n***This message has been sent from satimudesigns", $headers);
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $fname.\n",
+                "User Email: $email.\n",
+                 "User Message: $msg.\n";
+
+$to 		= 'brandonngandu@gmail.com';
+
+$headers	= "From: $fname \r\n";
+
+$headers    = "Reply To: $email \r\n";
+
+mail($to,$subject,$email_body,$headers);
+
+header("Location: contact.html")
